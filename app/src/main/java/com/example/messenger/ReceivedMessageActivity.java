67748efb7a86f4538fd3984854ac23239211b1f9
Denys <deniskole1@gmail.com>
2019-02-13 15:06:24 +0200

    Intent.ACTION_SEND

---
+++ app/src/main/java/com/example/messenger/ReceivedMessageActivity.java
@@ -0,0 +1,23 @@
+package com.example.messenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ReceivedMessageActivity extends AppCompatActivity {
+
+    private TextView textViewReceivedMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_received_message);
+
+        textViewReceivedMsg = findViewById(R.id.textViewReceivedMsg);
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra("msg");
+        textViewReceivedMsg.setText(msg);
+
+    }
+}
